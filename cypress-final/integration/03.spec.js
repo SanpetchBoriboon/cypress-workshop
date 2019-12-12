10bb8d9f0b88698a62ab43e35e2ee2dc8e15d040
deathsy <ks.srikittichai@gmail.com>
2019-12-12 16:48:11 +0700

    :sparkles: update 03 test cases

--- cypress-final/integration/03.spec.js
+++ cypress-final/integration/03.spec.js
@@ -4,12 +4,37 @@ describe("03", function() {
     cy.get("input").eq(0).type("adam");
     cy.get("input").eq(1).type("adam@gmail.com");
     cy.get("input").eq(2).type("Super_Secure_Password_1");
+    cy.get("form").get("button").click();
   });
+
+  it("[CLASS] should sign me up successfully", function() {
+    cy.visit("/register");
+    cy.get(".form-control .form-control-lg").eq(0).type("adam");
+    cy.get(".form-control .form-control-lg").eq(1).type("adam@gmail.com");
+    cy.get(".form-control .form-control-lg").eq(2).type("Super_Secure_Password_1");
+    cy.get(".btn btn-lg").get("button").click();
+  })
 
   it("[ID] should sign me up successfully", function() {
     cy.visit("/register");
     cy.get("#username").type("adam");
     cy.get("#email").type("adam@gmail.com");
     cy.get("#password").type("Super_Secure_Password_1");
+    cy.get("#register-submit").click()
+  })
+
+  it("[DATA-*] should sign me up successfully", function() {
+    cy.visit("/register");
+    cy.get("[data-test=username]").type("adam")
+    cy.get("[data-test=email]").type("adam@gmail.com");
+    cy.get("[data-test=password]").type("Super_Secure_Password_1");
+    cy.get("[data-test=register-submit]").click()
+  })
+
+  it("[SUBMIT_WITH_SPECIAL_KEY] should sign me up successfully", function() {
+    cy.visit("/register");
+    cy.get("[data-test=username]").type("adam")
+    cy.get("[data-test=email]").type("adam@gmail.com");
+    cy.get("[data-test=password]").type("Super_Secure_Password_1{enter}");
   })
 });
