4272fc8ad1f6ccacdf420aec79705937b7b212da
deathsy <ks.srikittichai@gmail.com>
2019-12-13 10:57:13 +0700

    :sparkles: adding 06.spec

---
+++ cypress-final/integration/06-1.spec.js
@@ -0,0 +1,21 @@
+describe("/register", function() {
+  beforeEach(function() {
+    cy.visit("/register")
+  })
+
+  it("should greet with `Sign Up`", function() {
+    cy.get("h1").contains("Sign Up");
+  });
+
+  it("should have a link that navigate to /register", function() {
+    cy.get("a").contains("Have an account?").should("have.attr", "href", "/login");
+  });
+
+  it("should sign me up successfully", function() {
+    cy.fixture("05.fixture").then(function(user) {
+      cy.get("[data-test=username]").type(user.username);
+      cy.get("[data-test=email]").type(user.email);
+      cy.get("[data-test=password]").type(user.password);
+    });
+  });
+});
---
+++ cypress-final/integration/06-2.spec.js
@@ -0,0 +1,20 @@
+describe("/signin", function() {
+  beforeEach(function() {
+    cy.visit("/sigin");
+  });
+
+  it("should greet with `Sign In`", function() {
+    cy.get("h1").contains("Sign In");
+  });
+
+  it("should have a link that navigate to login", function() {
+    cy.get("a").contains("Need an account?").should("have.attr", "href", "/register");
+  });
+
+  it("should sign me up successfully", function() {
+    cy.fixture("05.fixture").then(function(user) {
+      cy.get("[data-test=email]").type(user.email);
+      cy.get("[data-test=password]").type(user.password);
+    });
+  });
+});
