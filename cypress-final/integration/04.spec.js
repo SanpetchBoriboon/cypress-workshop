ec7665562d29ebceceec1beaad81c590d2977f0a
deathsy <ks.srikittichai@gmail.com>
2019-12-13 10:41:41 +0700

    :sparkles: adding 04.spec

---
+++ cypress-final/integration/04.spec.js
@@ -0,0 +1,17 @@
+describe("/signin", function() {
+  it("should greet with `Sign In`", function() {
+    cy.visit("/signin");
+    cy.get("h1").contains("Sign In");
+  });
+
+  it("should have a link that navigate to login", function() {
+    cy.get("/signin");
+    cy.get("a").contains("Need an account?").should("have.attr", "href", "/register");
+  });
+
+  it("should sign me up successfully", function() {
+    cy.get("/signin");
+    cy.get("[data-test=email]").type("adam@gmail.com")
+    cy.get("[data-test=password]").type("Super_Secure_Password_1{type}")
+  })
+});
