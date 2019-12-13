05593c108b4222ef0979f7b78c439f1863135ad9
deathsy <ks.srikittichai@gmail.com>
2019-12-13 11:12:36 +0700

    :sparkles: adding 08.spec

---
+++ cypress-final/integration/08.spec.js
@@ -0,0 +1,50 @@
+describe("/register", function() {
+  before(function() {
+    cy.task("npm run db:destroy && npm run db:migrate")
+  });
+
+  beforeEach(function() {
+    cy.visit("/register")
+  });
+
+  it("should greet with `Sign Up`", function() {
+    cy.get("h1").contains("Sign Up");
+  });
+
+  it("should have a link that navigate to /register", function() {
+    cy.get("a").contains("Have an account?").should("have.attr", "href", "/login");
+  });
+
+  it("should sign me up successfully", function() {
+    cy.fixture("05.fixture").then(function(user) {
+      cy.get("[data-test=username]").type(user.username);
+      cy.get("[data-test=email]").type(user.email);
+      cy.get("[data-test=password]").type(user.password);
+    });
+  });
+
+  it("should show error message when send same information", function() {
+    cy.server();
+    cy.route({
+      url: "/api/users",
+      method: "post",
+      status: 401,
+      response: {
+        error: {
+          username: "has been already taken.",
+          email: "has been already taken."
+        }
+      }
+    });
+
+    cy.fixture("05.fixture").then(function(user) {
+      cy.get("[data-test=username]").type(user.username);
+      cy.get("[data-test=email]").type(user.email);
+      cy.get("[data-test=password]").type(user.password);
+    });
+
+    //TODO: check for email error messages
+    //TODO: check for username error messages
+  });
+
+});
